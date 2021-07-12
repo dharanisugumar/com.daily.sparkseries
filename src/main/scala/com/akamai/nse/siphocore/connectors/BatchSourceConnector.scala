b4a1dc658a2464c72a6fca37cdb23a5391805932
dsugumar <dsugumar@akamai.com>
2021-07-12 10:25:01 +0530

    BEX-850, Implementation

--- src/main/scala/com/akamai/nse/siphocore/connectors/BatchSourceConnector.scala
+++
@@ -1,8 +0,0 @@
-package com.akamai.nse.siphocore.connectors
-
-import com.akamai.nse.siphocore.common.service.AbstractServiceTrait
-import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment}
-
-trait BatchSourceConnector[T] extends AbstractServiceTrait with Serializable {
-  def generate(env: ExecutionEnvironment): DataSet[T]
-}
--- src/main/scala/com/akamai/nse/siphocore/model/ProcedureInfo.scala
+++
@@ -1,8 +0,0 @@
-package com.akamai.nse.siphocore.model
-
-case class ProcedureInfo(
-                         proc_name: String,
-                         db_name: String,
-                         inputs: List[String],
-                         outputs: List[String]
-                       )
